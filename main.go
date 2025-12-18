8f224d8d02b24c5e70b6fbbcc02bdb36abf656c1
Lim-ZY <tcmpiano03@gmail.com>
2025-12-18 10:32:12 +0800

    added godotenv

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
   "fmt"
   "log"
   "net/http"
-  //"os"
+  "os"
   //"os/signal"
   "context"
   "github.com/lim-zy/CVWO-web-forum/internal/router"
   "github.com/lim-zy/CVWO-web-forum/internal/database"
+  "github.com/joho/godotenv"
 )
 
 func main() {
   //ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
   //defer cancel()
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatalln("Error loading .env file")
+  }
 
   db, err := database.GetDB(context.Background())
   if err != nil {
@@ -21,6 +26,11 @@ func main() {
   }
 
   r := router.Setup(db)
-  fmt.Println("Listening on port 8000 at http://localhost:8000 ...")
-  log.Fatalln(http.ListenAndServe(":8000", r))
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8000"
+  }
+  fmt.Printf("Listening on port %v at http://localhost:%v ...\n", port, port)
+  addr := ":" + port
+  log.Fatalln(http.ListenAndServe(addr, r))
 }
